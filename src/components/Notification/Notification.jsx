e65a312a9fe91d1f762fe10ac50ebfdeb8ab43b0
feoalexei <feoalexei@ukr.net>
2023-01-18 22:02:32 +0200

    finished Feedback Task

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,3 @@
+export const Notification = ({ message }) => <h3>{message}</h3>;
+
+export default Notification;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import css from './Section.module.css';
+
+const Section = ({ title, children }) => {
+  return (
+    <section className={css.section}>
+      <h2 className={css.sectionTitle}>{title}</h2>
+      {children}
+    </section>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Section;
